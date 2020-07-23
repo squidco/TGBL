d89f0c136f09dae52edf29bfdeebb95e832a39e0
AnthonyPillow <anthonypillow18@gmail.com>
2020-07-23 08:46:54 -0700

    forgot to commit these yesterday

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+const PORT = process.env.PORT || 8080;
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+app.get("/ping", function (req, res) {
+  return res.send("pong");
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(PORT, function () {
+  console.log(`listening on ${PORT}`)
+});
---
+++ src/components/Nav/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./style.css"
+
+function Nav() {
+    return(
+        <div className="row bar">
+            <div className="col">
+                <p>Logo goes here</p>
+            </div>
+            <div className="col">
+                <p>hamburger goes here</p>
+            </div>
+        </div>
+    )
+}
+
+export default Nav
