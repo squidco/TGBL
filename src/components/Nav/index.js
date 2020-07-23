cbe7b7f1485b828ee1aba94fbfaebfda0df892f5
AnthonyPillow <anthonypillow18@gmail.com>
2020-07-23 09:50:00 -0700

    Added a place holder image for my logo for now and created a favicon and updated the manifest

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,11 +1,12 @@
 import React from "react"
+import logo from "../../assets/images/Squid.jpg"
 import "./style.css"
 
 function Nav() {
     return(
         <div className="row bar">
             <div className="col">
-                <p>Logo goes here</p>
+                <img src={logo} alt="logo goes here" className="logo"></img>
             </div>
             <div className="col">
                 <p>hamburger goes here</p>
