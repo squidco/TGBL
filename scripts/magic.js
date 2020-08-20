a38805f496181c8d2fef3be5c4edb6f2c2c0d351
AnthonyPillow <anthonypillow18@gmail.com>
2020-08-20 13:14:35 -0700

    Added onto the magic.js script to where it makes an axios call for every el in the smol spell array and returns the result. Next step is getting to seed the db with that information which is just a subsequent axios call.

--- scripts/magic.js
+++ scripts/magic.js
@@ -1,17 +1,31 @@
 const mongoose = require("mongoose");
 const db = require("../models");
+const axios = require("axios");
 
-var beegSpellList = [];
-var smolSpellList = [];
+var beegSpellList = ["Thunderwave"];
 
 function convertShit() {
+  var smolSpellList = [];
   beegSpellList.forEach((element) => {
     var spellName = element.toLowerCase().replace(" ", "-");
 
     smolSpellList.push(spellName);
   });
+  lookUpShit(smolSpellList);
+}
+
+function lookUpShit(list) {
+  console.log(list);
+  list.forEach((el) => {
+    axios
+      .get(`https://www.dnd5eapi.co/api/spells/${el}/`)
+      .then(function (res) {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 }
 
 convertShit();
-
-console.log(smolSpellList);
