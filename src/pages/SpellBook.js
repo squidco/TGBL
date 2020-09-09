1bdfde2f10849040ae9cecf86b834e69c07b8152
AnthonyPillow <anthonypillow18@gmail.com>
2020-09-09 16:50:25 -0700

    Started to develop the spellbook page which will allow players to choose their class, level, and if they are multiclassing and bring up their spells.

--- src/pages/SpellBook.js
+++ src/pages/SpellBook.js
@@ -1,22 +1,49 @@
 import React, { useState } from "react";
 import "./style.css";
-import spellList from "../components/spellList";
+import spellList from "../components/spellList/index";
 
 function SpellBook() {
   const [playerInfo, setPlayerInfo] = useState({});
 
   const [spells, setSpells] = useState([]);
 
-  return (
-    
+  function handleFormInput(event) {
+    event.preventDefault();
+    const { name, value } = event.target;
+    setPlayerInfo({ ...playerInfo, [name]: value });
+    console.log(playerInfo);
+  }
 
-    <div>
-      <spellList
-        playerClass={playerInfo.playerClass}
-        playerLevel={playerInfo.playerLevel}
-        listOfSpells={spells}
-      />
-    </div>
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    console.log("submitted");
+  }
+
+  return (
+    <>
+      <form>
+        <label for="playerclass">Choose your class:</label>
+        <select onChange={handleFormInput} id="playerClass" name="playerClass">
+          <option value="Bard">Bard</option>
+          <option value="Cleric">Cleric</option>
+          <option value="Druid">Druid</option>
+          <option value="Paladin">Paladin</option>
+          <option value="Ranger">Ranger</option>
+          <option value="Sorcerer">Sorcerer</option>
+          <option value="Warlock">Warlock</option>
+          <option value="Wizard">Wizard</option>
+        </select>
+        <button onClick={handleFormSubmit}>Submit</button>
+      </form>
+
+      <div>
+        <spellList
+          playerClass={playerInfo.playerClass}
+          playerLevel={playerInfo.playerLevel}
+          listOfSpells={spells}
+        />
+      </div>
+    </>
   );
 }
 
