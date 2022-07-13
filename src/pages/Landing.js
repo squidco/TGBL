82ada594e9317061354529e0214f4d876e26e338
Anthony Pillow <anthonypillow18@gmail.com>
2022-07-12 19:03:47 -0700

    Added the function generate slot ids which generates an array with all the ids that I will add to the dynamically generated elements.

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -5,10 +5,13 @@ import NextButton from "../components/NextButton";
 import "./style.css";
 
 function Landing() {
+  //State for the css animation that is triggered in the changePage function
   const [transition, setTransitionState] = useState({
     start: false,
   });
 
+  //This function may be used in many places so I may make it a util function
+  //Triggers the animation for this page and changes the url afterwords to the new page
   function changePage(page) {
     if (transition.start === false) {
       setTransitionState({
@@ -31,10 +34,13 @@ function Landing() {
     >
       <div className="item-a">
         <p className="title">Welcome to The Good Boy List.</p>
-        <p className="title">
-          This app is undergoing development.
+        <p className="title">This app is undergoing development.</p>
+        <p className="words">
+          The goal now is to just track spell slots first and then expand to
+          tracking your own spells. To avoid lawsuits you will only be able to
+          track them locally on your pc. So you will have to put in the spells
+          yourself unfortunatly.
         </p>
-        <p className="words">The goal now is to just track spell slots first and then expand to tracking your own spells. To avoid lawsuits you will only be able to track them locally on your pc. So you will have to put in the spells yourself unfortunatly.</p>
         {/* <p className="words">
           If you feel lost, fret not. This realm assists spellcasters with their
           more mundane duties so they focus on sliging spells and being
@@ -51,11 +57,14 @@ function Landing() {
           I am working on that.
         </p> */}
         {/* <p className="">Now are you ready to start your adventure?</p> */}
-        <NextButton text="Next" click={changePage} />
-        <button onClick={() => changePage("/chooseclass")}>SpellSlots</button>
+        {/* <NextButton text="Next" click={changePage} /> */}
+        <button onClick={() => changePage("/spellslots")}>SpellSlots</button>
       </div>
     </div>
   );
+
+  //Additional notes: I may want to make the button that I click to change pages a component
+  //but I feel like that could trigger a bunch of problems with getting the animation to work and changing the page
 }
 
 export default Landing;
--- src/pages/SpellSlots.js
+++ src/pages/SpellSlots.js
@@ -1,8 +1,113 @@
+import { render } from "@testing-library/react";
 import React, { useState } from "react";
 import "./style.css";
 
-function SpellSlots(){
-    return <div>Theres nothing here</div>
+function SpellSlots() {
+  //State for the css animation that is triggered in the changePage function
+  const [transition, setTransitionState] = useState({
+    start: false,
+    slotStart: false,
+  });
+
+  //State for tracking the amount of player spell slots and level
+  const [player, setPlayer] = useState({
+    playerName: "Fortnite Default Skin",
+    playerLevel: 1,
+    highestSlot: 1,
+  });
+
+  //This function may be used in many places so I may make it a util function
+  //Triggers the animation for this page and changes the url afterwords to the new page
+  function changePage(page) {
+    if (transition.start === false) {
+      setTransitionState({
+        start: true,
+      });
+      setTimeout(() => {
+        window.location.href = page;
+      }, 1000);
+    }
+  }
+
+  //handles the changes for forum input
+  function handleFormInput(event) {
+    event.preventDefault();
+    const { name, value } = event.target;
+    setPlayer({ ...player, [name]: value });
+  }
+
+  function showSpellSlotInputs() {
+    console.log("slotChange");
+    render(<input placeholder="peepeepoopoo"></input>);
+  }
+
+  // Handles the form submit and adds entries to local storage
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    console.log("submitted");
+    console.log(player);
+    generateSlotIds(player.highestSlot);
+    localStorage.setItem(player.playerName, JSON.stringify(player));
+  }
+
+  function generateSlotIds(highestSlot) {
+    const slotArray = [];
+    for (var i = 0; i < highestSlot; i++) {
+      var number = `lv${highestSlot - i}`;
+      slotArray.push(number);
+    }
+    generateSlotInputs(slotArray);
+  }
+
+  function generateSlotInputs(slotArray) {
+    const slotInputs = slotArray.map((level) => (
+      <input id={level} name={level} key={level} type="number"></input>
+    ));
+    console.log(slotInputs)
+    return (slotArray.map((level) => (
+      <input id={level} name={level} key={level} type="number"></input>
+    )));
+  }
+
+  return (
+    <div
+      className={
+        transition.start === true
+          ? "landing-page-grid fade-out"
+          : "landing-page-grid"
+      }
+      id="landing-page-container"
+    >
+      <div className="item-a">
+        <form>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            onChange={handleFormInput}
+            id="playerName"
+            name="playerName"
+          ></input>
+          <input
+            type="number"
+            placeholder="Player level?"
+            onChange={handleFormInput}
+            id="playerLevel"
+            name="playerLevel"
+          ></input>
+          <input
+            type="number"
+            placeholder="Highest level spellslot?"
+            onChange={handleFormInput}
+            id="highestSlot"
+            name="highestSlot"
+          ></input>
+          <button onClick={handleFormSubmit}>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+  //Additional notes: The idea here is to reveal an input as they fill out inputs up to nine.
+  //Probably going to be janky though so I might switch the approach
 }
 
-export default SpellSlots
+export default SpellSlots;
