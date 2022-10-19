fc56e6057f431fec118ad00d4bb87157a48bce38
Anthony Pillow MacBook <anthonypillow18@gmail.com>
2022-10-18 21:13:40 -0700

    Added a line of documentation in the modal component

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css"
 
 function Modal({children}) {
+    // What ever is passed in for children will be rendered inside the modal
 return (
     <div className="modal-container">
         {children}
