83646b03321c7e111bb724d5510d3d034fa4c087
Anthony Pillow MacBook <anthonypillow18@gmail.com>
2023-02-27 22:07:04 -0800

    Updated styling for login page and nav bar

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -8,46 +8,49 @@ function LoginPage() {
         password: ""
     })
 
-    function handleFormInput(event){
+    function handleFormInput(event) {
         event.preventDefault();
-        const {name, value} = event.target;
-        setLoginForm({ ...loginForm, [name]: value})
+        const { name, value } = event.target;
+        setLoginForm({ ...loginForm, [name]: value })
     }
 
-    function handleSubmit(){
+    function handleSubmit() {
         axios({
             method: "POST",
             url: "/api/auth/login",
             data: loginForm
         }).then((response) => {
             console.log(response)
-            if(response.status === 200){
+            if (response.status === 200) {
                 document.cookie = `token=${response.data.token}; Secure;`
-            } 
+            }
         })
     }
 
     return (
-        <div className="container">
-            <div className="form-group">
-                <label htmlFor="email" className="m-1 words">Email</label>
-                <input
-                    id="email"
-                    name="email"
-                    onChange={handleFormInput}
-                    className="m-1 words"
-                ></input>
-            </div>
-            <div className="form-group">
-                <label htmlFor="password" className="m-1 words">Password</label>
-                <input
-                    id="password"
-                    name="password"
-                    onChange={handleFormInput}
-                    className="m-1 words"
-                ></input>
-            </div>
-            <button onClick={handleSubmit} className="m-1 words">Login</button>
+        <div className="text-center login-container">
+            <form className="form-signin">
+                <div className="form-group">
+                    <label htmlFor="email" className="m-1 words">Email</label>
+                    <input
+                        id="email"
+                        name="email"
+                        onChange={handleFormInput}
+                        className="m-1 words form-input"
+                    ></input>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password" className="m-1 words">Password</label>
+                    <input
+                        id="password"
+                        name="password"
+                        onChange={handleFormInput}
+                        className="m-1 words form-input"
+                    ></input>
+                </div>
+                <button onClick={handleSubmit} className="m-1 words">Login</button>
+
+            </form>
         </div>
     )
 }
