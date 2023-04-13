60302fdba3d2c1deeef700614c9edfae5a6bc8f0
Anthony Pillow MacBook <anthonypillow18@gmail.com>
2023-04-13 01:05:29 -0700

    commenting out more spell related routes and whatnot

--- routes/apiRoutes/spellRoutes.js
+++ routes/apiRoutes/spellRoutes.js
@@ -1,12 +1,12 @@
-const router = require("express").Router()
-const spellController = require("../../controllers/spellController")
+// const router = require("express").Router()
+// const spellController = require("../../controllers/spellController")
 
 
-router.route("/")
-    .get(spellController.findAll)
-    .post(spellController.createNewSpell)
+// router.route("/")
+//     .get(spellController.findAll)
+//     .post(spellController.createNewSpell)
 
-router.route("/:class")
-    .get(spellController.findClassSpells)
+// router.route("/:class")
+//     .get(spellController.findClassSpells)
 
-module.exports = router
+// module.exports = router
