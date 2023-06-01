3b078d4f3d368c7826e99cd4950293d48682a4e7
Anthony Pillow MacBook <anthonypillow18@gmail.com>
2023-06-01 11:47:07 -0700

    Added an edit button to the character cards on the characters page and changed some of the redirect logic so it was easier to work with.

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -1,21 +1,15 @@
 import React, { useState, useEffect } from "react";
 import PlayerForm from "../components/PlayerForm";
 import "./style.css";
-import { Link, Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import PlayerSearchForm from "../components/PlayerSearchForm";
 import axios from "axios";
 import AuthService from "../services/AuthService";
 
 function Characters() {
+  const [characterList, setCharacterList] = useState([]);
 
-  const [transition, setTransitionState] = useState({
-    start: false,
-    slotStart: false,
-  });
-
-  const [characterList, setCharacterList] = useState([])
-
-  const [redir, setRedir] = useState({ to: "" })
+  const [redir, setRedir] = useState({ to: "" });
 
   // get user's character data on load
   useEffect(() => {
@@ -23,39 +17,49 @@ function Characters() {
     async function getCharacterData() {
       try {
         const { data } = await axios.get("/api/characters", {
-          headers: { "authorization": AuthService.getToken() }
-        })
-        // Sets the character list state 
-        setCharacterList(data)
+          headers: { authorization: AuthService.getToken() },
+        });
+        // Sets the character list state
+        setCharacterList(data);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
     }
     // Calls the function written in the useEffect to get characterData
-    getCharacterData()
-  }, [])
+    getCharacterData();
+  }, []);
 
   // Handles the user clicking on a character card to go to that character
   function handlePlayerClick(event) {
-    event.preventDefault()
-    setRedir({ to: event.target.parentNode.dataset.character })
+    event.preventDefault();
+    setRedir({
+      to: "/playerdisplay/" + event.target.parentNode.dataset.character,
+    });
   }
 
   async function handleDelete(event) {
-    event.preventDefault()
-    event.stopPropagation()
+    event.preventDefault();
+    event.stopPropagation();
     try {
-      const { data } = await axios.delete(`/api/characters/${event.target.parentNode.dataset.character}`,
-        { headers: { "authorization": AuthService.getToken() } }
-      )
-      setCharacterList(data)
-    } catch(err) {
-      console.log(err)
+      const { data } = await axios.delete(
+        `/api/characters/${event.target.parentNode.dataset.character}`,
+        { headers: { authorization: AuthService.getToken() } }
+      );
+      setCharacterList(data);
+    } catch (err) {
+      console.log(err);
     }
   }
 
+  function handleEdit(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    setRedir({ to: "/edit/" + event.target.parentNode.dataset.character });
+  }
+
   return (
-    <div className={`mt-3 container op-1 ${transition.start === true ? "op-0" : null}`}>
+    <div className={`mt-3 container op-1`}>
       <section className="row">
         <div className="col">
           <PlayerSearchForm />
@@ -64,18 +68,23 @@ function Characters() {
         </div>
         <div className="col" onClick={handlePlayerClick}>
           <>
-            {redir.to && <Redirect push to={`/playerdisplay/${redir.to}`} />}
+            {redir.to && <Redirect push to={redir.to} />}
             {characterList.map((el) => (
               <div className="character-group" data-character={el.playerName}>
-                <button className="words" onClick={handleDelete}>X</button>
+                <button className="words" onClick={handleDelete}>
+                  X
+                </button>
+                <button className="words" onClick={handleEdit}>
+                  Edit
+                </button>
                 <h1 className="title">{el.playerName}</h1>
                 <p className="words pl-3">Level: {el.playerLevel}</p>
               </div>
             ))}
           </>
         </div>
-      </section >
-    </div >
+      </section>
+    </div>
   );
 }
 
