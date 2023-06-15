cf07d2c2ad6b15dbb2382529e3a72e5c85a58fc6
Anthony Pillow MacBook <anthonypillow18@gmail.com>
2023-06-14 23:08:48 -0700

    Fixed the edit button on the character cards.

--- client/src/components/CharacterCard.jsx
+++ client/src/components/CharacterCard.jsx
@@ -4,6 +4,7 @@ import AuthService from "../services/AuthService";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
 import { useState } from "react";
+import { Redirect } from "react-router-dom";
 
 export default function CharacterCard({ character, setCharacters }) {
   //redirect state
@@ -27,24 +28,27 @@ export default function CharacterCard({ character, setCharacters }) {
   function handleEdit(event) {
     event.preventDefault();
     event.stopPropagation();
-
+    console.log("EDIT BUTTON", character.playerName);
     setRedir({ to: "/edit/" + character.playerName });
   }
 
   return (
-    <div className="character-group" data-character={character.playerName}>
-      <button
-        className="words m-1"
-        data-character={character.playerName}
-        onClick={handleDelete}
-      >
-        <FontAwesomeIcon icon={icon({ name: "trash", style: "solid" })} />
-      </button>
-      <button className="words m-1" onClick={handleEdit}>
-        Edit
-      </button>
-      <h1 className="title">{character.playerName}</h1>
-      <p className="words pl-3">Level: {character.playerLevel}</p>
-    </div>
+    <>
+      {redir.to && <Redirect push to={redir.to} />}
+      <div className="character-group" data-character={character.playerName}>
+        <button
+          className="words m-1"
+          data-character={character.playerName}
+          onClick={handleDelete}
+        >
+          <FontAwesomeIcon icon={icon({ name: "trash", style: "solid" })} />
+        </button>
+        <button className="words m-1" onClick={handleEdit}>
+          Edit
+        </button>
+        <h1 className="title">{character.playerName}</h1>
+        <p className="words pl-3">Level: {character.playerLevel}</p>
+      </div>
+    </>
   );
 }
