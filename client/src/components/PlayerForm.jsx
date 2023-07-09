8e57170d60748e2884734977b592ae10933d9c24
Alex Nguyen <alex.nguyen369@gmail.com>
2023-07-09 15:38:31 -0700

    Fixed minor file path errors

--- client/src/components/PlayerForm.jsx
+++ client/src/components/PlayerForm.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import PopUp from "../PopUp";
+import PopUp from "./PopUp";
 import { Redirect } from "react-router-dom";
-import SpellSlotInputs from "../SpellSlotInputs";
+import SpellSlotInputs from "./SpellSlotInputs";
 import axios from "axios";
 import AuthService from "../services/AuthService";
 import "../styles/PlayerForm.css"
