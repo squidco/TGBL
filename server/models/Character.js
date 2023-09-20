17bda4c67b7c35befb1591c68bdcbc7fc2f0fb0f
Anthony Pillow MacBook <anthonypillow18@gmail.com>
2023-09-20 16:47:44 -0700

    updated character model with more information

--- server/models/Character.js
+++ server/models/Character.js
@@ -9,7 +9,39 @@ const CharacterSchema = new mongoose.Schema({
     trim: true,
     lowercase: true,
   },
-  characterLevel: Number,
+  stats: {
+    strength: {
+        type: Number,
+        default: 8
+    },
+    dexterity: {
+        type: Number,
+        default: 8
+    },
+    constitution: {
+        type: Number,
+        default: 8
+    },
+    wisdom: {
+        type: Number,
+        default: 8
+    },
+    intelligence: {
+        type: Number,
+        default: 8
+    },
+    charisma: {
+        type: Number,
+        default: 8
+    },
+  },
+  characterLevel: { type: Number, default: 1 },
+  classes: [
+    {
+      name: { type: String, default: "Fighter" },
+      level: { type: Number, default: 1 },
+    },
+  ],
   highestSlot: Number,
   armorClass: Number,
   inventory: [Object],
@@ -18,7 +50,7 @@ const CharacterSchema = new mongoose.Schema({
     fails: { type: Number, min: 0, max: 3 },
     successes: { type: Number, min: 0, max: 3 },
   },
-  proficiencyBonus: { type: Number, default: 2},
+  proficiencyBonus: { type: Number, default: 2 },
   numberOfSlots: [
     {
       id: Number,
