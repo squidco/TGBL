779e77fd4edc00acc7038e4db4431298c2e20e96
Alex Nguyen <atn012@ucsd.edu>
2023-11-08 15:29:51 -0800

    Something

--- client/src/components/NameCard.jsx
+++ client/src/components/NameCard.jsx
@@ -4,4 +4,28 @@ import { Redirect } from "react-router-dom";
 import SpellSlotInputs from "./SpellSlotInputs";
 import axios from "axios";
 import AuthService from "../services/AuthService";
-import "../styles/PlayerForm.css"
+
+function NameCard(props) {
+
+  // State for the character's name, classes, and levels
+  const [character, setCharacter] = useState({
+    characterName: "Unnamed",
+    characterLevel: null,
+    characterClasses: []
+  })
+
+  // Define an async function to get character data using an api call
+  async function getCharacterData() {
+
+  }
+
+  // Set the character state
+  useEffect(() => {
+    getCharacterData()
+  })
+
+
+}
+
+
+export default NameCard
